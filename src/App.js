b42b7d58e08d9eabc2d2bc1fc73b78cfea2b17f0
Pitri Indrayani <indrayanipitri@gmail.com>
2022-07-11 09:34:26 +0700

    Reusable Component & Props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import React, {useState} from 'react';
+import Card from './Card';
 
 function App() {
-  
+  return (
+    <>
+    <div className="py-5">
+        <div className="container">
+            <div className="row">
+                <div className="col-4">
+                    <Card content= {{ 
+                        imageUrl: "http://placekitten.com/g/300/200",
+                        title: "Postingan Pertama",
+                        date: "Published, 20 Februari 2021"
+                     }} />
+                </div>
+                <div className="col-4">
+                    <Card content= {{ 
+                        imageUrl: "http://placekitten.com/300/200",
+                        title: "Postingan Kedua",
+                        date: "Published, 4 Januari 2019"
+                     }} />
+                </div>
+                <div className="col-4">
+                    <Card content= {{ 
+                        imageUrl: "http://placekitten.com/g/300/200",
+                        title: "Postingan Ketiga",
+                        date: "Published, 27 Januri 2018"
+                     }} />
+                </div>
+            </div>
+        </div>
+    </div>
+    </>
+  )
 }
 
 export default App;
---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Card(props){
+    return(
+        <>
+            <div className="card">
+                <img src={props.content.imageUrl} alt="" />
+                <div className="card-body">
+                    <h4>{props.content.title}</h4>
+                    <small className='text-muted'>{props.content.date}</small>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Card;
