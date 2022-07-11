48984b5eeddc089da900b6db80777efe0ee1f50f
Pitri Indrayani <indrayanipitri@gmail.com>
2022-07-11 15:27:23 +0700

    Fetching Data Dengan Useeffect

--- src/App.js
+++ src/App.js
@@ -1,38 +1,81 @@
-import React, {useState} from 'react';
-import Card from './Card';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <>
-    <div className="py-5">
-        <div className="container">
-            <div className="row">
-                <div className="col-4">
-                    <Card content= {{ 
-                        imageUrl: "http://placekitten.com/g/300/200",
-                        title: "Postingan Pertama",
-                        date: "Published, 20 Februari 2021"
-                     }} />
-                </div>
-                <div className="col-4">
-                    <Card content= {{ 
-                        imageUrl: "http://placekitten.com/300/200",
-                        title: "Postingan Kedua",
-                        date: "Published, 4 Januari 2019"
-                     }} />
-                </div>
-                <div className="col-4">
-                    <Card content= {{ 
-                        imageUrl: "http://placekitten.com/g/300/200",
-                        title: "Postingan Ketiga",
-                        date: "Published, 27 Januri 2018"
-                     }} />
+    const [identifier, setIdentifier] = useState(1)
+    const [user, setUser] = useState([])
+    const [loading, setLoading] = useState(false)  //ngebuat loading pas data blm tampil
+
+    const getUser = async () => {
+        setLoading(true)
+        try {
+            let response = await axios.get(`https://jsonplaceholder.typicode.com/users/${identifier}`)
+            setUser(response.data)
+            setLoading(false)
+        } catch(e) {
+            setLoading(true)
+            console.log(e.message)
+        }
+    }
+
+    useEffect(() => {
+        getUser()
+    }, [identifier])
+
+    return (
+        <>
+        <div className="py-5">
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-8">
+                        <input type="text" name="identifies" id="identifier" value={identifier} onChange={(e) => setIdentifier(e.target.value)} className='form-control' />
+                        
+                        {
+                            loading ? <div>Loading . . .</div> :
+                            <table className='table'>
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Username</th>
+                                    <th>Email</th>
+                                    <th>Website</th>
+                                    <th>Phone</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                                <tr> 
+                                    <td>{user.name}</td>
+                                    <td>{user.username}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.website}</td>
+                                    <td>{user.phone}</td>
+                                </tr>
+
+
+                                {/* {
+                                    users.map((user, index) => {
+                                        return (
+                                            <tr key={index}> 
+                                                <td>{user.name}</td>
+                                                <td>{user.username}</td>
+                                                <td>{user.email}</td>
+                                                <td>{user.website}</td>
+                                                <td>{user.phone}</td>
+                                            </tr>
+                                        )
+                                    })
+                                } */}
+                            </tbody>
+                        </table>
+                        }
+                       
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default App;
