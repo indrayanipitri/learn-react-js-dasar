bf9397527e7f8cab8669ed53d56a77512bf82f4c
Pitri Indrayani <indrayanipitri@gmail.com>
2022-07-11 10:17:48 +0700

    update alt

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ function Card(props){
     return(
         <>
             <div className="card">
-                <img src={props.content.imageUrl} alt="" />
+                <img src={props.content.imageUrl} alt="cat" />
                 <div className="card-body">
                     <h4>{props.content.title}</h4>
                     <small className='text-muted'>{props.content.date}</small>
